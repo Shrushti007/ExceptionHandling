5278aa171f0e27855ad734d880fdd7fadb83a0d2
Ankit Jagtap <ankit.jagtap50@gmail.com>
2024-06-27 17:30:10 +0530

    Add project files.

---
+++ ExceptionHandlingDemo/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.Web.Mvc;
+using ExceptionHandlingDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Controller = Microsoft.AspNetCore.Mvc.Controller;
+
+namespace ExceptionHandlingDemo.Controllers
+{
+    public class HomeController :Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HandleError(ExceptionType = typeof(DivideByZeroException), View = "Error")]
+        public IActionResult Index()
+        {
+            int a = 10;
+            int b = 0;
+            int c = a / b;
+            ViewBag.result = c;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+    }
+}
